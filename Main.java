9e1c71b4c218145d6cbfd89238f2c611a28533b6
Konovalova1924 <150925187+Konovalova1924@users.noreply.github.com>
2024-05-10 22:08:56 +0300

    Create Main.java

---
+++ Main.java
@@ -0,0 +1,50 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Проанализировав файл input.txt, сообщаю, что:   ");
+        fruitMoving.counterWord();
+        fruitMoving.wordLengthMax();
+        fruitMoving.frequencyWord();
+
+    }
+}
+// Результат:
+// Проанализировав файл input.txt, сообщаю, что:   
+// Всего слов в файле ==> 1082
+// Самое длинное слово из перечисленных ниже ==> Raspberries
+// В файле встречаются следующие слова:
+// Apple ==> 75 раз
+// Banana ==> 42 раз
+// Beet ==> 6 раз
+// Blackberry ==> 66 раз
+// Blueavocado ==> 3 раз
+// Blueberry ==> 99 раз
+// Cabbage ==> 24 раз
+// Carrot ==> 6 раз
+// Cherry ==> 69 раз
+// Durian ==> 39 раз
+// Eggplant ==> 6 раз
+// Grape ==> 6 раз
+// Kiwi ==> 39 раз
+// Lemon ==> 72 раз
+// Mango ==> 111 раз
+// Onion ==> 6 раз
+// Orange ==> 39 раз
+// Pear ==> 108 раз
+// Potato ==> 6 раз
+// Pumpkin ==> 6 раз
+// Raspberries ==> 102 раз
+// Strawberry ==> 72 раз
+// Tomato ==> 12 раз
+// Turnip ==> 9 раз
+// fruits ==> 3 раз
+// melon ==> 6 раз
+// mushroom ==> 4 раз
+// nectarine ==> 9 раз
+// peach ==> 9 раз
+// pepper ==> 1 раз
+// persimmon ==> 3 раз
+// plum ==> 9 раз
+// quince ==> 3 раз
+// tangerine ==> 6 раз
+// watermelon ==> 6 раз
